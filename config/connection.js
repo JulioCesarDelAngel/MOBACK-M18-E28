eb31af574b2e78bfc4fb43f86a12cc1894e6a012
Julio C. MW11 <sysa.mail@gmail.com>
2023-01-27 17:55:59 -0600

    se eliminan useNewUrlParserm useUnifiedTopology

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ const connectionString =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/studentsDB';
 
 connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  //useNewUrlParser: true,
+  //useUnifiedTopology: true,
 });
 
 module.exports = connection;
